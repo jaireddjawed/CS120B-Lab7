59d668f07f4b0e0aef0f07d4c16d588e6cbee271
Jaired Jawed <jairedjawed@icloud.com>
2021-05-02 20:29:04 -0700

    Completed Part 1

---
+++ source/main.c
@@ -0,0 +1,39 @@
+/*	Author: lab
+ *  Partner(s) Name: Jaired Jawed
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+
+	unsigned short my_short = 0x00;
+	ADC_init();
+
+    /* Insert your solution below */
+    while (1) {
+	my_short = ADC;
+	// First 8 bits onto PORTB
+	PORTB = (char) (((1 << 8) - 1) & (my_short >> 0));
+	// last 2 bits onto PORTD
+	PORTD = (char)(my_short >> 8);
+    }
+ 
+    return 1;
+}
